21d139775c193b78445c09d64c013c0770e84e28
David Greenberg <dgreenberg@rti.com>
2026-06-04 12:24:46 -0400

    Remove top-level run_rti_view wrapper

--- run_rti_view.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Launch rti_view from the workspace root.
-# Usage: ./run_rti_view.sh -d 0 -t Square -f x -m plot --history 30
-
-exec "$(dirname "$0")/tools/rti_view/run_rti_view.sh" "$@"
